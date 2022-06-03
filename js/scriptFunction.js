f68969aff65c4cb46a683ec0854f04d0f6500e25
Alexandr Older <alex.older77@gmail.com>
2022-06-03 17:32:08 +0300

    functions part 2

--- js/scriptFunction.js
+++ js/scriptFunction.js
@@ -30,7 +30,77 @@
 
 // logger();
 
-const calc = (a, b) => {
-    console.log('Hello!');
-    return a + b;
-};
+// const calc = (a, b) => {
+//     console.log('Hello!');
+//     return a + b;
+// };
+
+/* const usdCurr = 29;
+const discount = 0.9;
+
+function convert (amount, curr) {
+    return curr * amount;
+}
+
+function promotion (result) {
+    console.log(result * discount);
+}
+
+// const res = convert(500, usdCurr);
+// promotion(res);
+
+promotion(convert(500, usdCurr)); */
+
+/* function test() {
+    for (let i =0; i < 5; i++) {
+        console.log(i);
+        if (i === 3) return
+    }
+    console.log('Done');
+}
+
+test(); */
+
+/* function sayHello(name) {
+    return 'Привет, ' + name + '!';
+    // console.log('Привет, ' + name + '!');
+}
+
+sayHello("Антон"); */
+
+// function returnNeighboringNumbers(integer) {
+//     return [integer-1, integer, integer+1];
+// }
+
+// returnNeighboringNumbers(5);
+
+/* function getMathResult(num, times) {
+    if (typeof(times) !== 'number' || times <= 0) {
+        return num;
+    }
+
+    let str = '';
+
+    for (let i = 1; i <= times; i++) {
+        if (i === times) {
+            str += `${num * i}`;
+            // Тут без черточек в конце
+        } else {
+            str += `${num * i}---`;
+            // Это тоже самое, что и
+            // str = str + num * i + "---"
+        }
+    }
+
+    return str;
+}
+
+getMathResult(10, 5); */
+
+
+
+// function getMathResult() {
+//     for (i = 0; i < 0; i++) {
+        
+//     }
+// }
